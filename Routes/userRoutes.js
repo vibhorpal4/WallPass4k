0ab19aa9a390c7df482bc1935d2e7cc6af21610a
vibhorpal4 <vibhorpal4@gmail.com>
2022-01-15 22:35:37 +0530

    update userUpdate route

--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -7,7 +7,7 @@ const router = express.Router();
 router.get("/", userController.getAllUser);
 router.get("/:username", userController.getUser);
 router.delete("/:username/delete", authMiddleware, userController.deleteUser);
-router.patch("/:username/update", authMiddleware, userController.updateUser);
+router.put("/:username/update", authMiddleware, userController.updateUser);
 router.get("/users/stats", userController.getUserStats);
 router.put("/:id/follow", authMiddleware, userController.followUser);
 router.put("/:id/unfollow", authMiddleware, userController.unfollowUser);
